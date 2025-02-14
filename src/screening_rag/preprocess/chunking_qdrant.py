846707b941c8bdffc5cb28639fcc46cc2990bf51
Nancycho <nancy1xn1xn@gmail.com>
2025-02-14 17:55:44 +0800

    version2 of inserting to qdrant

--- src/screening_rag/preprocess/chunking_qdrant.py
+++ src/screening_rag/preprocess/chunking_qdrant.py
@@ -8,87 +8,74 @@ embeddings = OpenAIEmbeddings(
     model="text-embedding-3-large",
     dimensions=1024
 )
+
+from qdrant_client import QdrantClient, models
+client = QdrantClient(url="http://localhost:6333")
+
+# create collections
+from qdrant_client import QdrantClient, models
+client = QdrantClient(url="http://localhost:6333")
+client.create_collection(
+    collection_name="cnn_news_chunk_vectors",
+    vectors_config=models.VectorParams(size=1024, distance=models.Distance.COSINE),
+)
+
+assert client.collection_exists(collection_name="cnn_news_chunk_vectors")
+# collection_info = client.get_collection("cnn_news_chunk_vectors")
+# print(collection_info)
+
 db=MySQLdb.connect(host="127.0.0.1", user = "root", password="my-secret-pw",database="my_database")
 cur=db.cursor()
 cur.execute("select PK_CHUNK, text, Article_PK from my_database.CHUNK_CNN_NEWS")
 
-#只embed text
-for row in cur.fetchall():
+
+#只embed text, 其他插入payload
+for index, row in enumerate(cur.fetchall()):
     row: t.Tuple
     pk_chunk, text, article_pk = row
     text_openai_vectors = embeddings.embed_documents(text)
-    openai_vectors: t.List[List[float]]
+    text_openai_vectors: t.List[List[float]]
+    for vector_index, each_text_openai_vectors in enumerate(text_openai_vectors):
+        unique_id = int(f"{index}{vector_index}")
+        # id = f"unique_id-{unique_id}"  # 假設 ID 應該是不同的
+        # print(id)
+        client.upsert(
+            collection_name="cnn_news_chunk_vectors",
+                points=[
+                    models.PointStruct(
+                        id=unique_id,
+                        payload={
+                        "pk_chunk": pk_chunk,
+                        "text": text,
+                        "article_pk": article_pk
+                        },
+                        vector=each_text_openai_vectors
+                    ),
+                ],
+        )
+
     # print(openai_vectors)
     # print(type(openai_vectors))
     # d = np.array(openai_vectors)
     # print(d.shape)
     # raise ValueError
 
-#先合併pk, text, article_pk向量
-# for row in cur.fetchall():
-#     row: t.Tuple
-#     openai_vectors = embeddings.embed_documents(str(row))
-#     openai_vectors: t.List[List[float]]
 
-    # print(openai_vectors)
-    # print(type(openai_vectors))
-    # d = np.array(openai_vectors)
-    # print(d.shape)
-    # raise ValueError
-
-# for row in cur.fetchall():
-#     row: t.Tuple
-#     pk_chunk, text, article_pk = row
-#     pk_chunk_openai_vectors = embeddings.embed_documents(str(pk_chunk))
-#     pk_chunk_openai_vectors: t.List[List[float]]
-#     text_openai_vectors = embeddings.embed_documents(text)
-#     text_openai_vectors: t.List[List[float]]
-#     article_pk_openai_vectors = embeddings.embed_documents(str(article_pk))
-#     article_pk_openai_vectors: t.List[List[float]]
-    # print(pk_chunk_openai_vectors)
-    # print(type(pk_chunk_openai_vectors))
-    # print(text_openai_vectors)
-    # print(article_pk_openai_vectors)
-    # print(f"Embedding Dimension: {len(pk_chunk_openai_vectors[0])}")
-    # print(f"Embedding Dimension: {len(text_openai_vectors[0])}")
-    # print(f"Embedding Dimension: {len(article_pk_openai_vectors[0])}")
-
-# a = np.array(pk_chunk_openai_vectors)
-# b = np.array(text_openai_vectors)
-# c = np.array(article_pk_openai_vectors)
-# print(a.shape)
-# print(b.shape)
-# print(c.shape)
-# raise ValueError
-
-
-#create collections
-from qdrant_client import QdrantClient, models
-client = QdrantClient(url="http://localhost:6333")
-# client.create_collection(
-#     collection_name="cnn_news_chunk_vectors",
-#     vectors_config=models.VectorParams(size=1024, distance=models.Distance.COSINE),
-# )
-
-# assert client.collection_exists(collection_name="cnn_news_chunk_vectors")
-# collection_info = client.get_collection("cnn_news_chunk_vectors")
-# print(collection_info)
-
-
-#一次塞一組 chunk_pk, text, article_id
-for index, vector_list_pk_chunk in enumerate(openai_vectors):
-    client.upsert(
-        collection_name="cnn_news_chunk_vectors",
-            points=[
-                models.PointStruct(
-                    id=index,
-                    payload={
-                    "data": "chunk_id and text and article_id"
-                    },
-                    vector=vector_list_pk_chunk
-                ),
-            ],
-    )
+#一次塞一組 text 去向量資料庫/ payload紀錄 chunk_pk, article_id
+# for index, vector_list_pk_chunk in enumerate(openai_vectors):
+    # client.upsert(
+    #     collection_name="cnn_news_chunk_vectors",
+    #         points=[
+    #             models.PointStruct(
+    #                 id=index,
+    #                 payload={""""
+    #                 "pk_chunk": pk_chunk
+    #                 "article_pk": article_pk
+    #                 """},
+    #                 vector=vector_list_pk_chunk
+    #             ),
+    #         ],
+    # )
 
     # ids.append(idx)
 # test =[-0.01984037458896637, 0.01448314543813467, -0.019018713384866714]
