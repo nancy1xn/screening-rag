2c05c7d2cd57c2d36efc143663e6da7bcd86a479
Nancycho <nancy1xn1xn@gmail.com>
2025-02-17 12:50:44 +0800

    add chunking_qdrant version1

--- src/screening_rag/preprocess/chunking_qdrant.py
+++ src/screening_rag/preprocess/chunking_qdrant.py
@@ -9,36 +9,30 @@ embeddings = OpenAIEmbeddings(
     dimensions=1024
 )
 
-from qdrant_client import QdrantClient, models
-client = QdrantClient(url="http://localhost:6333")
-
 # create collections
 from qdrant_client import QdrantClient, models
 client = QdrantClient(url="http://localhost:6333")
-client.create_collection(
-    collection_name="cnn_news_chunk_vectors",
-    vectors_config=models.VectorParams(size=1024, distance=models.Distance.COSINE),
-)
+# client.create_collection(
+#     collection_name="cnn_news_chunk_vectors",
+#     vectors_config=models.VectorParams(size=1024, distance=models.Distance.COSINE),
+# )
 
-assert client.collection_exists(collection_name="cnn_news_chunk_vectors")
+# assert client.collection_exists(collection_name="cnn_news_chunk_vectors")
 # collection_info = client.get_collection("cnn_news_chunk_vectors")
 # print(collection_info)
 
 db=MySQLdb.connect(host="127.0.0.1", user = "root", password="my-secret-pw",database="my_database")
 cur=db.cursor()
 cur.execute("select PK_CHUNK, text, Article_PK from my_database.CHUNK_CNN_NEWS")
-
-
 #只embed text, 其他插入payload
-for index, row in enumerate(cur.fetchall()):
+unique_id = 0
+for row in cur.fetchall():
     row: t.Tuple
     pk_chunk, text, article_pk = row
     text_openai_vectors = embeddings.embed_documents(text)
     text_openai_vectors: t.List[List[float]]
-    for vector_index, each_text_openai_vectors in enumerate(text_openai_vectors):
-        unique_id = int(f"{index}{vector_index}")
-        # id = f"unique_id-{unique_id}"  # 假設 ID 應該是不同的
-        # print(id)
+    for each_text_openai_vectors in text_openai_vectors:
+        print(f'unique_id:{unique_id}')
         client.upsert(
             collection_name="cnn_news_chunk_vectors",
                 points=[
@@ -53,6 +47,7 @@ for index, row in enumerate(cur.fetchall()):
                     ),
                 ],
         )
+        unique_id+=1
 
     # print(openai_vectors)
     # print(type(openai_vectors))
@@ -61,39 +56,8 @@ for index, row in enumerate(cur.fetchall()):
     # raise ValueError
 
 
-#一次塞一組 text 去向量資料庫/ payload紀錄 chunk_pk, article_id
-# for index, vector_list_pk_chunk in enumerate(openai_vectors):
-    # client.upsert(
-    #     collection_name="cnn_news_chunk_vectors",
-    #         points=[
-    #             models.PointStruct(
-    #                 id=index,
-    #                 payload={""""
-    #                 "pk_chunk": pk_chunk
-    #                 "article_pk": article_pk
-    #                 """},
-    #                 vector=vector_list_pk_chunk
-    #             ),
-    #         ],
-    # )
 
-    # ids.append(idx)
-# test =[-0.01984037458896637, 0.01448314543813467, -0.019018713384866714]
-# client.upsert(
-#     collection_name="testing",
-#         points=[
-#             models.PointStruct(
-#                 id=1,
-#                 payload={
-#                 "color": "red",
-#                 },
-#                 vector= test,
-#             ),
-#         ],
-# )
-
-
-# #search
+#search
 # results = client.search(
 #     collection_name="cnn_news",
 #     query_vector=openai_vectors[0],
@@ -122,3 +86,4 @@ for index, row in enumerate(cur.fetchall()):
 # print(results, next_page)
 
 #CURL -L -X GET 'http://localhost:6333/collections/cnn_news_chunk_vectors/points/1'
+#curl -X DELETE "http://localhost:6333/collections/cnn_news_chunk_vectors"
