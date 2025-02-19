26bcdfea586b118ecd957906e356d84a7387bb6f
Nancycho <nancy1xn1xn@gmail.com>
2025-02-19 18:44:30 +0800

    added note

--- src/screening_rag/preprocess/search_qdrant.py
+++ src/screening_rag/preprocess/search_qdrant.py
@@ -42,18 +42,17 @@ class Relevance(BaseModel):
 # Create an instance of the model and enforce the output structure
 model = ChatOpenAI(model="gpt-4o", temperature=0) 
 structured_model = model.with_structured_output(Relevance)
+
 # Define the system prompt
 system = """You are a helpful assistant to provide relevence score based on how much useful content the answer contains in relation to the original question. 
             The score is within the range of 0 to 1."""
 
 saved_chunks = []
 
+
 for question_key, question_value in question.items():
     question_openai_vectors = embeddings.embed_documents(question_value)
     question_openai_vectors: t.List[List[float]]
-    
-# print(question_openai_vectors)
-# print(type(question_openai_vectors))
 
 # search
     for question_index, question_vector in enumerate(question_openai_vectors):
@@ -63,28 +62,12 @@ for question_key, question_value in question.items():
             limit=3
         )
         relevence_score_open_ai= structured_model.invoke([SystemMessage(content=system)]+[HumanMessage(content=str(search_results))])
-        print('search_results',search_results)
+        # print('search_results',search_results)
+        # print(relevence_score_open_ai)
 
-        # print(type(search_results))
-        print(relevence_score_open_ai)
-        # raise ValueError
-        text_collection = []
-        for result in search_results.points: #search_results有三個, result等於每一個scoredpoint
-            
-            # print('index_result', index_result)
-            # print('single_result', result)
-            # print('type of result', type(result))
-            # print('single_result_text',result.payload["text"])
-
+        text_collection = [] #注意每次三個蒐集完都要歸零不然會一直累積
+        for result in search_results.points: #search results是QueryReponse type 要先用point取出attribute, search_results有三個, result等於每一個scoredpoint
             text_collection.append(result.payload["text"])
-       
-        # print(text_collection)
-        # raise ValueError
-        #         print("Original question",question_value[question_index])
-        #         print("Search Results:", search_results)
-        #         print(relevence_score_open_ai)
-        #         print("result0",result[0], "result1",result[1])        
-        #         raise ValueError
 
         saved_chunks.append({
             "question": question_value[question_index],
@@ -95,31 +78,9 @@ for question_key, question_value in question.items():
         # saved_chunks.append({
         #     "question": question_value[question_index],
         #     "text": search_results,
-        #     #  result.payload["text"] or result["payload"]["text"]
+        #     #  result.payload["text"] or result["payload"]["text"] #因為是QueryResponse不能用[]來取直, 要先取points, 再取Nametuple-payload["text"]
         #     "score": relevence_score_open_ai
         #     })
         
 for chunk in saved_chunks:
     print(chunk)
-# print(saved_chunks)
-
-
-#search
-# for question_vector in question_openai_vectors:
-#     results = client.query_points(
-#         collection_name="cnn_news_chunk_vectors",
-#         query=question_vector,
-        # query_filter=models.Filter(
-        # must=[
-        #     models.FieldCondition(
-        #         key="text",
-        #         match=models.MatchValue(
-        #             value="Binance",
-        #         ),
-        #     )
-        # ]
-        # ),
-    #     limit=5
-    # )
-
-# print(results)
