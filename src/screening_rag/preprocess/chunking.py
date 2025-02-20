7cab3aee560712cbbe3ced819ff4cdbf84694179
Nancycho <nancy1xn1xn@gmail.com>
2025-02-20 20:35:39 +0800

    change pk in table

--- src/screening_rag/preprocess/chunking.py
+++ src/screening_rag/preprocess/chunking.py
@@ -9,17 +9,17 @@ import MySQLdb
 db=MySQLdb.connect(host="127.0.0.1", user = "root", password="my-secret-pw",database="my_database")
 cur=db.cursor()
 # cur.execute("DROP TABLE CHUNK_CNN_NEWS;")
-# cur.execute("""
-#     CREATE TABLE CHUNK_CNN_NEWS (
-#         PK_CHUNK BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-#         text VARCHAR(1000),
-#         start_position INT,
-#         end_position INT,
-#         Article_PK BIGINT UNSIGNED NOT NULL,
-#         PRIMARY KEY(PK_CHUNK),
-#         FOREIGN KEY (Article_PK) REFERENCES CNN_NEWS(Article_PK)
-#     );
-# """)
+cur.execute("""
+    CREATE TABLE CHUNK_CNN_NEWS (
+        ID_CH BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        text VARCHAR(1000),
+        start_position INT UNSIGNED,
+        end_position INT UNSIGNED,
+        ID_A BIGINT UNSIGNED NOT NULL,
+        PRIMARY KEY(ID_CH),
+        FOREIGN KEY (ID_A) REFERENCES CNN_NEWS(ID_A)
+    );
+""")
 
 def chunk_text(text:str) -> t.Iterable[str]:
     character_splitter = RecursiveCharacterTextSplitter(
@@ -29,7 +29,7 @@ def chunk_text(text:str) -> t.Iterable[str]:
         add_start_index=True
     )
     text_split = character_splitter.split_text(text)
-    token_splitter = SentenceTransformersTokenTextSplitter()
+    token_splitter = SentenceTransformersTokenTextSplitter(tokens_per_chunk=256)
 
     for section in text_split:
         #  chunks.extend(token_splitter.split_text(section))
@@ -37,14 +37,14 @@ def chunk_text(text:str) -> t.Iterable[str]:
 
 db=MySQLdb.connect(host="127.0.0.1", user = "root", password="my-secret-pw",database="my_database")
 cur=db.cursor()
-cur.execute("select maintext, Article_PK from my_database.CNN_NEWS")
+cur.execute("select maintext, ID_A from my_database.CNN_NEWS")
 for row in cur.fetchall():
     row: t.Tuple
     maintext, article_pk = row
     chunks_example = chunk_text(maintext)
     for chunk in chunks_example:
             cur.execute(
-                """INSERT INTO my_database.CHUNK_CNN_NEWS (text, Article_PK)
+                """INSERT INTO my_database.CHUNK_CNN_NEWS (text, ID_A)
                 VALUES (%s, %s)""",
                 (chunk,
                 article_pk)
