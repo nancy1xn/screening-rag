93e3c6b3619db0f4c53f8d6b19a3ec19772bafdc
Nancycho <nancy1xn1xn@gmail.com>
2025-03-18 17:44:36 +0800

    copy

--- test/streamlit_demo.py
+++ test/streamlit_demo.py
@@ -2,6 +2,16 @@ import streamlit as st
 from screening_rag.preprocess.search_qdrant_adverse_media import gen_report
 import pandas as pd
 
+
+if 'key' not in st.session_state:
+    st.session_state['key'] = ""
+
+# if 'markdown_content1' not in st.session_state:
+#     st.session_state.markdown_content1 =""
+
+# if 'markdown_content2' not in st.session_state:
+#     st.session_state.markdown_content2 =""
+
 def render_markdown(contents, appendices):
     contents_split_by_line=list(map(lambda x: f"\n - {x}", contents))
     final_contents= ",".join(contents_split_by_line)
@@ -14,13 +24,31 @@ def render_markdown(contents, appendices):
     return final_contents, final_appendices
 
 st.write("# Adverse Media Report")
-entity_name = st.text_input("Entity Name", help="The keyword used to search CNN news.")
+entity_name = st.text_input("Entity Name", help="The keyword used to search CNN news.", key="content_input")
+st.session_state.key = st.session_state.content_input
 
 def gen_adverse_media_report(entity_name):
-
+    
     if st.button("generate report"):
         content, appendix = gen_report(entity_name)
         final_contents, final_appendices = render_markdown(content, appendix)
+        
+        # st.session_state.markdown_content1 =f"""
+
+        #     ## Adverse Information Report Headline
+
+        #     {final_contents}
+        #     """
+        # st.markdown(st.session_state.markdown_content1)
+
+        # st.session_state.markdown_content2 =f"""
+
+        #     ## Appendix
+
+        #     {final_appendices}
+        #     """
+        # st.markdown(st.session_state.markdown_content2)
+
 
         st.markdown(
             f"""
@@ -40,6 +68,7 @@ def gen_adverse_media_report(entity_name):
 
         merged_content = f"# Adverse Media Report\n\n## Adverse Information Report Headline\n{final_contents}\n\n## Appendix\n{final_appendices}"
 
+
         st.download_button(
         label="Download Markdown",
         data=merged_content,
@@ -48,9 +77,13 @@ def gen_adverse_media_report(entity_name):
         icon=":material/download:",
         )
 
+    # if st.session_state.markdown_content1:
+    #     st.markdown(st.session_state.markdown_content1)
+    
+    # if st.session_state.markdown_content2:
+    #     st.markdown(st.session_state.markdown_content2)
+
+
+
 
 gen_adverse_media_report(entity_name)
-
-    # st.write(
-    #     pd.DataFrame(gen_report(entity_name))
-    # )
