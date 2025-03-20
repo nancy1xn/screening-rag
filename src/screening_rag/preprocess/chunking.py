639cfcd6eb6ec3d777c45aa7607087fa31542ed2
Nancycho <nancy1xn1xn@gmail.com>
2025-03-20 17:50:59 +0800

    update

--- src/screening_rag/preprocess/chunking.py
+++ src/screening_rag/preprocess/chunking.py
@@ -9,20 +9,6 @@ from newsplease.NewsArticle import NewsArticle
 from screening_rag.preprocess.chunking_qdrant import process_and_insert_chunks_to_cnn_news_chunk_vectors
 
 def insert_chunk_table(news_article:NewsArticle, article_id):
-    db=MySQLdb.connect(host="127.0.0.1", user = "root", password="my-secret-pw",database="my_database")
-    cur=db.cursor()
-    # cur.execute("DROP TABLE CHUNK_CNN_NEWS;")
-    # cur.execute("""
-    #     CREATE TABLE CHUNK_CNN_NEWS (
-    #         ID BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    #         text VARCHAR(1000),
-    #         start_position INT UNSIGNED,
-    #         end_position INT UNSIGNED,
-    #         parent_article_id BIGINT UNSIGNED NOT NULL,
-    #         PRIMARY KEY(ID),
-    #         FOREIGN KEY (parent_article_id) REFERENCES CNN_NEWS(ID)
-    #     );
-    # """)
 
     def chunk_text(text:str) -> t.Iterable[str]:
         """Split text into several chunks which are smaller units
@@ -43,16 +29,10 @@ def insert_chunk_table(news_article:NewsArticle, article_id):
         token_splitter = SentenceTransformersTokenTextSplitter(tokens_per_chunk=256)
 
         for section in text_split:
-            #  chunks.extend(token_splitter.split_text(section))
             yield token_splitter.split_text(section)
 
     db=MySQLdb.connect(host="127.0.0.1", user = "root", password="my-secret-pw",database="my_database")
     cur=db.cursor()
-    # cur.execute("select maintext, ID from my_database.CNN_NEWS")
-    # for row in cur.fetchall():
-    #     row: t.Tuple
-
-        # maintext, article_pk = row
     chunks = chunk_text(news_article.maintext)
     for chunk in chunks:
             cur.execute(
@@ -62,9 +42,6 @@ def insert_chunk_table(news_article:NewsArticle, article_id):
                 article_id)
             )
             chunk_id = cur.lastrowid
-            # print(chunk)
-            # print(type(chunk))
-            # raise ValueError
             process_and_insert_chunks_to_cnn_news_chunk_vectors(chunk, article_id, chunk_id)
     
     db.commit()
