13455d2f4e1de9ddcf3f7a4edeb6f33afc004fce
Nancycho <nancy1xn1xn@gmail.com>
2025-03-25 17:03:19 +0800

    use ruff for auto-formatter

--- src/screening_rag/preprocess/streamlit_reporter.py
+++ src/screening_rag/preprocess/streamlit_reporter.py
@@ -1,29 +1,35 @@
 import streamlit as st
-from screening_rag.preprocess.cnn_news_searcher import gen_report1
+
 from screening_rag.preprocess.cnn_crime_event_searcher import gen_report
+from screening_rag.preprocess.cnn_news_searcher import gen_report1
+
 
 def render_markdown(contents, appendices):
+    contents_split_by_line = list(map(lambda x: f"\n - {x}", contents))
+    final_contents = ",".join(contents_split_by_line)
 
-    contents_split_by_line=list(map(lambda x: f"\n - {x}", contents))
-    final_contents= ",".join(contents_split_by_line)
-
-    contents_in_markdown_format= list(map(lambda x:f"[{x[0]} {x[1]}]({x[2]})", appendices))
-    contents_in_markdown_format_split_by_line= list(map(lambda x: f"\n - {x}",contents_in_markdown_format))
+    contents_in_markdown_format = list(
+        map(lambda x: f"[{x[0]} {x[1]}]({x[2]})", appendices)
+    )
+    contents_in_markdown_format_split_by_line = list(
+        map(lambda x: f"\n - {x}", contents_in_markdown_format)
+    )
     final_appendices = ",".join(contents_in_markdown_format_split_by_line)
 
     return final_contents, final_appendices
 
+
 st.write("# Adverse Media Report")
 entity_name = st.text_input("Entity Name", help="The keyword used to search CNN news.")
 
+
 def gen_adverse_media_report(entity_name):
-    
     if st.button("generate report"):
-        background, appendix1= gen_report1(entity_name)
+        background, appendix1 = gen_report1(entity_name)
         content, appendix = gen_report(entity_name)
-        final_background, final_appendices1= render_markdown(background, appendix1)
+        final_background, final_appendices1 = render_markdown(background, appendix1)
         final_contents, final_appendices = render_markdown(content, appendix)
-        
+
         st.markdown(
             f"""
 
@@ -61,11 +67,12 @@ def gen_adverse_media_report(entity_name):
         merged_content = f"# Adverse Media Report\n\n## Background\n{final_background}\n\n## Appendix1\n{final_appendices1}\n\n## Adverse Information Report Headline\n{final_contents}\n\n## Appendix2\n{final_appendices}"
 
         st.download_button(
-        label="Download Markdown",
-        data=merged_content,
-        file_name="data.md",
-        mime="text/markdown",
-        icon=":material/download:",
+            label="Download Markdown",
+            data=merged_content,
+            file_name="data.md",
+            mime="text/markdown",
+            icon=":material/download:",
         )
+
 
 gen_adverse_media_report(entity_name)
