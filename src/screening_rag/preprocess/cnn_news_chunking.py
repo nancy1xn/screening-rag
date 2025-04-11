e75e2c4e46e1f7d67f23a436a73e9e5b64fe04ff
Nancycho <nancy1xn1xn@gmail.com>
2025-04-11 11:34:41 +0800

    divided chunk_text & insert_chunk_table into two functions

--- src/screening_rag/preprocess/cnn_news_chunking.py
+++ src/screening_rag/preprocess/cnn_news_chunking.py
@@ -16,25 +16,26 @@ from screening_rag.preprocess.cnn_news_chunking_to_qdrant import (
 )
 
 
+def chunk_text(text: str) -> t.Iterable[str]:
+    """Split text into several chunks which are smaller units
+
+    Recursively split the text by characters, and then use a sentence model tokenizer to further split the text into chunk tokens.
+    Chunks are yielded one group at a time ,allowing iteration over each chunks group.
+
+    Args:
+        text(str): The text to be split into chunks.
+    """
+    character_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n"], chunk_size=256, chunk_overlap=50, add_start_index=True
+    )
+    text_splits = character_splitter.split_text(text)
+    token_splitter = SentenceTransformersTokenTextSplitter(tokens_per_chunk=256)
+
+    for section in text_splits:
+        yield token_splitter.split_text(section)
+
+
 def insert_chunk_table(news_article: NewsArticle, article_id):
-    def chunk_text(text: str) -> t.Iterable[str]:
-        """Split text into several chunks which are smaller units
-
-        Recursively split the text by characters, and then use a sentence model tokenizer to further split the text into chunk tokens.
-        Chunks are yielded one group at a time ,allowing iteration over each chunks group.
-
-        Args:
-            text(str): The text to be split into chunks.
-        """
-        character_splitter = RecursiveCharacterTextSplitter(
-            separators=["\n"], chunk_size=256, chunk_overlap=50, add_start_index=True
-        )
-        text_split = character_splitter.split_text(text)
-        token_splitter = SentenceTransformersTokenTextSplitter(tokens_per_chunk=256)
-
-        for section in text_split:
-            yield token_splitter.split_text(section)
-
     mysqldb_pw = os.getenv("MYSQLDB_PW")
     db = MySQLdb.connect(
         host="127.0.0.1", user="root", password=mysqldb_pw, database="my_database"
