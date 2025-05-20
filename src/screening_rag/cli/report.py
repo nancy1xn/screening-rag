9b8d4c94bcfe096f59b887ae8fb52d5f3d900e34
Nancycho <nancy1xn1xn@gmail.com>
2025-05-20 14:03:06 +0800

    revise to two routers (report&read)

--- src/screening_rag/cli/report.py
+++ src/screening_rag/cli/report.py
@@ -21,77 +21,106 @@ def render_markdown(contents, appendices):
     return final_contents, final_appendices
 
 
-def report():
-    print("in the function")
-    st.write("# Adverse Media Report")
-    st.markdown(
-        """
-<div style="font-size:16px; color:gray; line-height:1.6">
-This is a demo system that automatically generates AML (Anti-Money Laundering) reports based on selected keywords.  
-It provides background information and past adverse media headlines for financial entities. Powered by natural language processing, the system is designed to assist in preliminary compliance risk assessments.
-
-Currently supported keywords:
-- JPMorgan Chase & Co.  
-- Binance Holdings Ltd.  
-- Deutsche Bank  
-
-⚠️ Please note that this is a demonstration platform. Data is limited and not updated in real time.
-</div>
-""",
-        unsafe_allow_html=True,
-    )
-    entity_name = st.text_input(
-        "Entity Name", help="The keyword used to search CNN news."
-    )
-    if st.button("generate report"):
-        background, appendix1 = generate_background_report(entity_name)
-        content, appendix = generate_crime_events_report(entity_name)
-        final_background, final_appendices1 = render_markdown(background, appendix1)
-        final_contents, final_appendices = render_markdown(content, appendix)
-
+def router():
+    query_params = st.query_params
+    page = query_params.get("page", "read")
+    if page == "read":
+        print("in the function")
+        st.write("# Adverse Media Report")
         st.markdown(
-            f"""
-
-            ## Background
-
-            {final_background}
             """
+    <div style="font-size:16px; color:gray; line-height:1.6">
+    This is a demo system that automatically generates AML (Anti-Money Laundering) reports based on selected keywords.  
+    It provides background information and past adverse media headlines for financial entities. Powered by natural language processing, the system is designed to assist in preliminary compliance risk assessments.
+
+    Currently supported keywords:
+    - JPMorgan Chase & Co.  
+    - Binance Holdings Ltd.  
+    - Deutsche Bank  
+
+    ⚠️ Please note that this is a demonstration platform. Data is limited and not updated in real time.
+    </div>
+    """,
+            unsafe_allow_html=True,
+        )
+        entity_name = st.text_input(
+            "Entity Name", help="The keyword used to search CNN news."
         )
 
+    elif page == "report":
+        print("in the function")
+        st.write("# Adverse Media Report")
         st.markdown(
-            f"""
-
-            ## Appendix1
-
-            {final_appendices1}
             """
+    <div style="font-size:16px; color:gray; line-height:1.6">
+    This is a demo system that automatically generates AML (Anti-Money Laundering) reports based on selected keywords.  
+    It provides background information and past adverse media headlines for financial entities. Powered by natural language processing, the system is designed to assist in preliminary compliance risk assessments.
+
+    Currently supported keywords:
+    - JPMorgan Chase & Co.  
+    - Binance Holdings Ltd.  
+    - Deutsche Bank  
+
+    ⚠️ Please note that this is a demonstration platform. Data is limited and not updated in real time.
+    </div>
+    """,
+            unsafe_allow_html=True,
         )
-
-        st.markdown(
-            f"""
-
-            ## Adverse Information Report Headline
-
-            {final_contents}
-            """
+        entity_name = st.text_input(
+            "Entity Name", help="The keyword used to search CNN news."
         )
-        st.markdown(
-            f"""
-            ## Appendix2
+        if st.button("generate report"):
+            background, appendix1 = generate_background_report(entity_name)
+            content, appendix = generate_crime_events_report(entity_name)
+            final_background, final_appendices1 = render_markdown(background, appendix1)
+            final_contents, final_appendices = render_markdown(content, appendix)
 
-            {final_appendices}
-            """
-        )
+            st.markdown(
+                f"""
 
-        merged_content = f"# Adverse Media Report\n\n## Background\n{final_background}\n\n## Appendix1\n{final_appendices1}\n\n## Adverse Information Report Headline\n{final_contents}\n\n## Appendix2\n{final_appendices}"
+                ## Background
 
-        st.download_button(
-            label="Download Markdown",
-            data=merged_content,
-            file_name="data.md",
-            mime="text/markdown",
-            icon=":material/download:",
-        )
+                {final_background}
+                """
+            )
+
+            st.markdown(
+                f"""
+
+                ## Appendix1
+
+                {final_appendices1}
+                """
+            )
+
+            st.markdown(
+                f"""
+
+                ## Adverse Information Report Headline
+
+                {final_contents}
+                """
+            )
+            st.markdown(
+                f"""
+                ## Appendix2
+
+                {final_appendices}
+                """
+            )
+
+            merged_content = f"# Adverse Media Report\n\n## Background\n{final_background}\n\n## Appendix1\n{final_appendices1}\n\n## Adverse Information Report Headline\n{final_contents}\n\n## Appendix2\n{final_appendices}"
+
+            st.download_button(
+                label="Download Markdown",
+                data=merged_content,
+                file_name="data.md",
+                mime="text/markdown",
+                icon=":material/download:",
+            )
+
+    else:
+        st.warning("頁面不存在")
 
 
 def main():
@@ -104,4 +133,4 @@ def main():
 
 
 if __name__ == "__main__":
-    report()
+    router()
